1553f1d0245900441316767007d260cfe5c8ca66
Di Vrusa Florian <floriandv1996@gmail.com>
2017-11-07 14:20:34 +0100

    Fixing scripts, add copyright

--- Scripts/create_database.sql
+++ Scripts/create_database.sql
@@ -1,10 +1,18 @@
+-- Copyright (c) 2017 the original author or authors
+-- All rights reserved. This program and the accompanying materials
+-- are made available under the terms of the GNU Lesser Public License v2.1
+-- which accompanies this distribution, and is available at
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+--
+-- Creates the tables of the "public" schema.
+
 --Tables creation--
 CREATE TABLE IF NOT EXISTS public.appointment
 (
     id SERIAL PRIMARY KEY NOT NULL,
     description VARCHAR(500),
     name VARCHAR(100) NOT NULL,
-    duration TIME
+    duration TIMESTAMP
 );
 
 
@@ -15,7 +23,7 @@ CREATE TABLE IF NOT EXISTS public.customer
     lastname VARCHAR(100) NOT NULL,
     email VARCHAR(200),
     billing_address VARCHAR(200) NOT NULL,
-    phone VARCHAR(50),
+    phone VARCHAR(20),
     address VARCHAR(200)
 );
 
@@ -25,7 +33,7 @@ CREATE TABLE IF NOT EXISTS public.doctor
     id SERIAL PRIMARY KEY NOT NULL,
     firstname VARCHAR(100) NOT NULL,
     lastname VARCHAR(100) NOT NULL,
-    phone VARCHAR(50),
+    phone VARCHAR(20),
     specialty VARCHAR(40)
 );
 
@@ -36,8 +44,8 @@ CREATE TABLE IF NOT EXISTS public.dog
     description VARCHAR(500),
     dog_breed VARCHAR(50) NOT NULL,
     name VARCHAR(50) NOT NULL,
-    size INT,
-    weight INT
+    size FLOAT,
+    weight FLOAT
 );
 
 
@@ -56,7 +64,7 @@ CREATE TABLE IF NOT EXISTS public.secretary
     lastname VARCHAR(100) NOT NULL,
     email VARCHAR(200) NOT NULL,
     password VARCHAR(200) NOT NULL,
-    phone VARCHAR(50)
+    phone VARCHAR(20)
 );
 
 
