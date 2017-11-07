01096dd283070d8296e2717c8a7fe346a230c740
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2017-11-07 15:58:52 +0100

    Update login view Angular

--- src/main/resources/static/angular-cli/src/app/app.component.ts
+++ src/main/resources/static/angular-cli/src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
+    selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
