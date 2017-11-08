b1b1fffcece2533b8a6d4f344a45d7c2feb366c2
Guillaume Quittet <guillaume.quittet@gmail.com>
2017-11-08 22:07:26 +0100

    Make angular integration with spring

---
+++ src/main/java/be/heh/dogcare/Controllers/IndexController.java
@@ -0,0 +1,18 @@
+package be.heh.dogcare.Controllers;;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    /**
+     * @return index page
+     */
+    @GetMapping(value = "/")
+    @ResponseBody
+    public String index() {
+        return "Welcome to the index page!";
+    }
+}
