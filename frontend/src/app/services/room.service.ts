0db9c4ce2d5d92b211beb061b90657d10a840c9c
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2017-11-10 18:57:43 +0100

    Fix room service

--- frontend/src/app/services/room.service.ts
+++ frontend/src/app/services/room.service.ts
@@ -7,15 +7,15 @@ export class RoomService {
     constructor(private http: Http) { }
 
     getAll() {
-        return this.http.get('/api/doctor').map((response: Response) => response.json());
+        return this.http.get('/api/room').map((response: Response) => response.json());
     }
 
     getByName(name: string) {
-        return this.http.get('/api/doctor/search/findByName?name=' + name).map((response: Response) => response.json());
+        return this.http.get('/api/room/search/findByName?name=' + name).map((response: Response) => response.json());
     }
 
     getByDescription(description: string) {
-        return this.http.get('/api/doctor/search/findByDescription?description=' + description).map((response: Response) => response.json());
+        return this.http.get('/api/room/search/findByDescription?description=' + description).map((response: Response) => response.json());
     }
 
 }
