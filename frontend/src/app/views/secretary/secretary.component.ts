83f7143dd54cdb6a4594a74b956d7c78d268487b
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2017-12-19 16:40:18 +0100

    Fix secretary navbar

--- frontend/src/app/views/secretary/secretary.component.ts
+++ frontend/src/app/views/secretary/secretary.component.ts
@@ -15,13 +15,19 @@ export class SecretaryComponent implements OnInit {
     constructor(private secretaryService: SecretaryService) { }
 
     ngOnInit() {
+        window.sessionStorage.last_endpoint = '/secretary';
+        const navElems = document.getElementsByTagName('li');
+        for (let i = 0; i < navElems.length; i++) {
+            navElems[i].classList.remove('active');
+        }
+        document.getElementById('nav-secretary').classList.add('active');
 
         this.secretaryService.getAll().subscribe(
             data => {
                 this.secretaries = data['secretary'];
             },
             error => console.error(error)
-        )
+        );
     }
 
 }
