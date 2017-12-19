9a4d27dcad44445391b08b394f4379fbcd1bb84f
Di Vrusa Florian <floriandv1996@gmail.com>
2017-12-19 15:44:19 +0100

    add list for items (html)

--- frontend/src/app/views/secretary/secretary.component.ts
+++ frontend/src/app/views/secretary/secretary.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { SecretaryService } from '../../services/secretary.service';
+import { error } from 'selenium-webdriver';
 
 @Component({
     selector: 'app-secretary',
     templateUrl: './secretary.component.html',
-    styleUrls: ['./secretary.component.css']
+    styleUrls: ['./secretary.component.css'],
+    providers: [SecretaryService]
 })
 export class SecretaryComponent implements OnInit {
 
-    constructor() { }
+    secretaries: String[];
+
+    constructor(private secretaryService: SecretaryService) { }
 
     ngOnInit() {
-        window.sessionStorage.last_endpoint = '/secretary';
-        const navElems = document.getElementsByTagName('li');
-        for (let i = 0; i < navElems.length; i++) {
-            navElems[i].classList.remove('active');
-        }
-        document.getElementById('nav-secretary').classList.add('active');
+
+        this.secretaryService.getAll().subscribe(
+            data => {
+                this.secretaries = data['secretary'];
+            },
+            error => console.error(error)
+        )
     }
 
 }
