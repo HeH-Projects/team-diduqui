653eec0770b040e346785c78807df3b0c48f5b49
Di Vrusa Florian <floriandv1996@gmail.com>
2018-01-18 12:25:09 +0100

    Add component 'Add-doctor'

---
+++ frontend/src/app/views/add-doctor/add-doctor.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddDoctorComponent } from './add-doctor.component';
+
+describe('AddDoctorComponent', () => {
+  let component: AddDoctorComponent;
+  let fixture: ComponentFixture<AddDoctorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddDoctorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddDoctorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/views/add-doctor/add-doctor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-doctor',
+  templateUrl: './add-doctor.component.html',
+  styleUrls: ['./add-doctor.component.css']
+})
+export class AddDoctorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
